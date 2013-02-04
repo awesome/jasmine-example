3d214033dbf26da55aca0b77a0d2550323dd3b70
Vitaly Tatarintsev <Kalastiuz@gmail.com>
2013-02-04 06:37:43 +0200

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JasmineExample::Application.config.secret_token = 'd84c64ba423eedc2cc626dfae8945a9809e8fa271c462e102330e93e1644779152cf3db18b2abe72636f48968ed1ab5677acd4161ba68a0f7ae86442731f5ed0'
